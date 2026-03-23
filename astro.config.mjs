cd82e27e372fef767a59095775381fea420f2fc5
Leo0114 <leonel0614@gmail.com>
2026-03-23 17:28:51 -0600

    upadte: first config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,5 +11,12 @@ export default defineConfig({
 
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en"],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
 });
